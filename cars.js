c7620bf7181457e7bc5e4639e503c741752bd106
Robert McKenney <robert@mckenney.ca>
2019-01-31 11:07:06 -0500

    EX4 starter code

---
+++ cars.js
@@ -0,0 +1,25 @@
+module.exports = [{
+    id: 1,
+    make: 'Tesla',
+    model: 'S',
+    colour: 'Black'
+  },
+  {
+    id: 2,
+    make: 'Tesla',
+    model: '3',
+    colour: 'Indigo'
+  },
+  {
+    id: 3,
+    make: 'Tesla',
+    model: 'X',
+    colour: 'Brown'
+  },
+  {
+    id: 4,
+    make: 'Tesla',
+    model: 'Y',
+    colour: 'Silver'
+  }
+]
