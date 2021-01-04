79a0dccff884c7954670df34d0b3120603ff6122
Robert McKenney <robert@mckenney.ca>
2021-01-04 17:13:18 -0500

    Updated for W21

--- app.js
+++ app.js
@@ -1,89 +1,96 @@
 'use strict'
-
-const cars = require('./cars.js')
+// load dependencies
+const {cars} = require('./cars.js')
 const express = require('express')
+
+// create the express app
 const app = express()
 
+// configure express middleware
 app.use(express.json())
 
-app.get('/api/cars/', (req, res) => res.send({ data: cars }))
+// define routes
+app.get('/api/cars', (req, res) => res.send({data: cars}))
 
 app.get('/api/cars/:carId', (req, res) => {
-  const carId = parseInt(req.params.carId)
-  const car = cars.find(car => car.id === carId)
-  res.send({ data: car })
+  const car = cars.find(car => car.id === parseInt(req.params.carId))
+  res.send({data: car})
 })
 
 app.post('/api/cars', (req, res) => {
-  const { make, model, colour } = req.body
+  const {make, model, colour} = req.body
   const newCar = {
     id: Date.now(),
-    make: make,
-    model: model,
-    colour: colour
+    make,
+    model,
+    colour
   }
   cars.push(newCar)
-  res.status(201).send({ data: newCar })
+  res.status(201).send({data: newCar})
 })
 
 app.put('/api/cars/:carId', (req, res) => {
-  const carId = parseInt(req.params.carId)
-  const index = cars.findIndex(car => car.id === carId)
+  const id = parseInt(req.params.carId)
+  const index = cars.findIndex(car => car.id === id)
   if (index < 0) {
     res.status(404).send({
-      errors: [{
-        status: 'Not Found',
-        code: '404',
-        title: 'Resource does not exist',
-        description: `We could not find a car with id: ${carId}`
-      }]
+      errors: [
+        {
+          status: '404',
+          title: 'Resource does not exist',
+          description: `We could not find a car with id: ${id}`
+        }
+      ]
     })
   } else {
-    const { make, model, colour } = req.body
-    const updatedCar = { id: carId, make, model, colour }
+    const {make, model, colour} = req.body
+    const updatedCar = {id, make, model, colour}
     cars[index] = updatedCar
-    res.send({ data: updatedCar })
+    res.send({data: updatedCar})
   }
 })
 
 app.patch('/api/cars/:carId', (req, res) => {
-  const carId = parseInt(req.params.carId)
-  const index = cars.findIndex(car => car.id === carId)
+  const id = parseInt(req.params.carId)
+  const index = cars.findIndex(car => car.id === id)
   if (index < 0) {
     res.status(404).send({
-      errors: [{
-        status: 'Not Found',
-        code: '404',
-        title: 'Resource does not exist',
-        description: `We could not find a car with id: ${carId}`
-      }]
+      errors: [
+        {
+          status: '404',
+          title: 'Resource does not exist',
+          description: `We could not find a car with id: ${id}`
+        }
+      ]
     })
   } else {
-    const { id, ...theRest } = req.body
+    const {id, ...theRest} = req.body
     const updatedCar = Object.assign({}, cars[index], theRest)
     cars[index] = updatedCar
-    res.send({ data: updatedCar })
+    res.send({data: updatedCar})
   }
 })
 
 app.delete('/api/cars/:carId', (req, res) => {
-  const carId = parseInt(req.params.carId)
-  const index = cars.findIndex(car => car.id === carId)
+  const id = parseInt(req.params.carId)
+  const index = cars.findIndex(car => car.id === id)
   if (index < 0) {
     res.status(404).send({
-      errors: [{
-        status: 'Not Found',
-        code: '404',
-        title: 'Resource does not exist',
-        description: `We could not find a car with id: ${carId}`
-      }]
+      errors: [
+        {
+          status: '404',
+          title: 'Resource does not exist',
+          description: `We could not find a car with id: ${id}`
+        }
+      ]
     })
   } else {
-    const deletedCar = cars[index]
-    cars.splice(index, 1)
-    res.send({ data: deletedCar })
+    // splice returns an array of the removed items
+    const deletedCars = cars.splice(index, 1)
+    res.send({data: deletedCars[0]})
   }
 })
 
+// start listening for HTTP requests
 const port = process.env.port || 3030
 app.listen(port, () => console.log(`Server listening on port ${port} ...`))
--- cars.js
+++ cars.js
@@ -1,25 +1,8 @@
-module.exports = [{
-    id: 1,
-    make: 'Tesla',
-    model: 'S',
-    colour: 'Black'
-  },
-  {
-    id: 2,
-    make: 'Tesla',
-    model: '3',
-    colour: 'Indigo'
-  },
-  {
-    id: 3,
-    make: 'Tesla',
-    model: 'X',
-    colour: 'Brown'
-  },
-  {
-    id: 4,
-    make: 'Tesla',
-    model: 'Y',
-    colour: 'Silver'
-  }
+const cars = [
+  {id: 1, make: 'Tesla', model: 'S', colour: 'Black'},
+  {id: 2, make: 'Tesla', model: '3', colour: 'Red'},
+  {id: 3, make: 'Tesla', model: 'X', colour: 'Silver'},
+  {id: 4, make: 'Tesla', model: 'Y', colour: 'Chestnut Brown'}
 ]
+
+module.exports = {cars}
