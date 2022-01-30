475a2776d768db1158f4633e9f34a2165736df4a
Robert McKenney <robert@mckenney.ca>
2022-01-30 16:45:18 -0500

    update for w22 - JSON:API formatter

--- app.js
+++ app.js
@@ -9,24 +9,62 @@ const app = express()
 // configure express middleware
 app.use(express.json())
 
+/**
+ * Format the response data object according to JSON:API v1.0
+ * @param {string} type The resource collection name, e.g. 'cars'
+ * @param {Object} resource An instance object from that collection
+ * @returns
+ */
+function formatResponseData(type, resource) {
+  const {id, ...attributes} = resource
+  return {type, id, attributes}
+}
+
 // define routes
-app.get('/api/cars', (req, res) => res.send({data: cars}))
+app.get('/api/cars', (req, res) => {
+  res.json({data: cars.map(car => formatResponseData('cars', car))})
+})
 
 app.get('/api/cars/:carId', (req, res) => {
-  const car = cars.find(car => car.id === parseInt(req.params.carId))
-  res.send({data: car})
+  const id = parseInt(req.params.carId)
+  const car = cars.find(car => car.id === id)
+  if (!car) {
+    res.status(404).send({
+      errors: [
+        {
+          status: '404',
+          title: 'Resource does not exist',
+          description: `We could not find a car with id: ${id}`
+        }
+      ]
+    })
+  }
+  res.json({data: formatResponseData('cars', car)})
 })
 
 app.post('/api/cars', (req, res) => {
-  const {make, model, colour} = req.body
-  const newCar = {
-    id: Date.now(),
-    make,
-    model,
-    colour
+  const {data} = req.body
+  if (data?.type === 'cars') {
+    const newCar = {
+      ...data.attributes,
+      id: Date.now()
+    }
+    cars.push(newCar)
+    res.status(201).json({data: formatResponseData('cars', newCar)})
+  } else {
+    res.status(400).json({
+      errors: [
+        {
+          status: '400',
+          title: 'schema validation error',
+          detail: `Expected resource type to be 'Car', got '${data?.type}'`,
+          source: {
+            pointer: '/data/type'
+          }
+        }
+      ]
+    })
   }
-  cars.push(newCar)
-  res.status(201).send({data: newCar})
 })
 
 app.put('/api/cars/:carId', (req, res) => {
@@ -42,12 +80,14 @@ app.put('/api/cars/:carId', (req, res) => {
         }
       ]
     })
-  } else {
-    const {make, model, colour} = req.body
-    const updatedCar = {id, make, model, colour}
-    cars[index] = updatedCar
-    res.send({data: updatedCar})
   }
+  // process request
+  const updatedCar = {
+    ...req.body?.data?.attributes,
+    id
+  }
+  cars[index] = updatedCar
+  res.json({data: formatResponseData('cars', updatedCar)})
 })
 
 app.patch('/api/cars/:carId', (req, res) => {
@@ -63,12 +103,16 @@ app.patch('/api/cars/:carId', (req, res) => {
         }
       ]
     })
-  } else {
-    const {id, ...theRest} = req.body
-    const updatedCar = Object.assign({}, cars[index], theRest)
-    cars[index] = updatedCar
-    res.send({data: updatedCar})
   }
+  // process request
+  const updatedCar = Object.assign(
+    {},
+    cars[index],
+    req.body?.data?.attributes,
+    {id}
+  )
+  cars[index] = updatedCar
+  res.json({data: formatResponseData('cars', updatedCar)})
 })
 
 app.delete('/api/cars/:carId', (req, res) => {
@@ -84,11 +128,12 @@ app.delete('/api/cars/:carId', (req, res) => {
         }
       ]
     })
-  } else {
-    // splice returns an array of the removed items
-    const deletedCars = cars.splice(index, 1)
-    res.send({data: deletedCars[0]})
   }
+  const deletedCar = cars.splice(index, 1)[0]
+  res.json({
+    data: formatResponseData('cars', deletedCar),
+    meta: {message: `Car with id: ${id} successfully deleted.`}
+  })
 })
 
 // start listening for HTTP requests
